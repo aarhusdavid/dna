bb5e8a4613df1c7f0b19f56efb90847efb980ca3
aarhusdavid <aarhus@chapman.edu>
2019-09-13 19:55:43 +0000

    writing to new file

--- dna_main.cpp
+++ dna_main.cpp
@@ -7,27 +7,23 @@ using namespace std;
 int main(int argc, char** argv) //argc stands for command line arguments
 //argv number arguments in command line
 {
-    // string test = argv[1];
-    //this pulls the string from the command line
-    string file_name = "";
+    ofstream outputFile;
+    outputFile.open("davidaarhus.txt");
 
-    cout << "Enter file name: " << endl;
-    cin >> file_name; //console input; like the scanner class in java to read input from user
+    outputFile << argv[1];
+    string file_name = argv[1];
 
     dna d; //stored in the stack
 
-    cout << "number of command line arguments: " << argc << endl;
-    cout << "Name of the file: " << file_name << endl;
-    cout << "Strings in file: " << "\n---------------" << d.countLength(file_name) << endl;
-    cout << "Sum of DNA nucleotides: " << d.sumLength(file_name) << endl;
-    cout << "Average amount of nucleotides in each DNA strand: " << d.mean(file_name) << endl;
-    cout << "Variance: " << d.variance(file_name) << endl;
-    cout << d.probability(file_name) << endl;
-
-
+    outputFile << "Strings in file: " << "\n---------------" << d.countLength(file_name) << endl;
+    outputFile << "Sum of DNA nucleotides: " << d.sumLength(file_name) << endl;
+    outputFile << "Average amount of nucleotides in each DNA strand: " << d.mean(file_name) << endl;
+    outputFile << "Variance: " << d.variance(file_name) << endl;
+    outputFile << d.probability(file_name) << endl;
 
 
     cout << "progam exited successfully" << endl;
+    outputFile.close();
 
     return 0;
 }
