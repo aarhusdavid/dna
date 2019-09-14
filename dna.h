8d6440bb3d53033b9101795eb16c57027e5ce6e4
aarhusdavid <aarhus@chapman.edu>
2019-09-14 23:47:57 +0000

    finished

--- dna.h
+++ dna.h
@@ -7,5 +7,17 @@ class dna
 public:
     dna(); //constructor
     ~dna(); //destructor
-    // int countVowels(string input);
+    
+    //initialzes methods
+    string countLength(string input);
+    int sumLength(string input);
+    double mean(string input);
+    double variance(string input);
+    double stdv(string input);
+    string probability(string input);
+    string getNucleo();
+    int gaussin(double mean, double stdv);
+
+
+
 };
