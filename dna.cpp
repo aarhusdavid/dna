7c123af24b9d3d9ad55428078097f35cb153328b
aarhusdavid <aarhus@chapman.edu>
2019-09-14 21:29:34 +0000

    updated variance and stdv methods

--- dna.cpp
+++ dna.cpp
@@ -1,40 +1,32 @@
+//header file that declares all the files
 #include "dna.h"
+//allows for reading and writing files
 #include <iostream>
 #include <fstream>
+//allows calculations
 #include <string>
 #include <cmath>
 
+//allows you to print
 using namespace std;
 
+//constructor
 dna::dna()
 {
 
 }
+
+//destructor
 dna::~dna()
 {
     cout << "object deleted" << endl;
 }
 
-string dna::countLength(string input)
-{
-    string nextLine;
-    string lines;
-    ifstream infile;
-    infile.open(input);
-    while (infile.peek() != EOF)
-    {
-        getline(infile,nextLine); // Saves the line in STRING.
-        lines = lines + "\n" + nextLine;
-    }
-    lines = lines + "\n---------------";
-    return lines;
-    infile.close();
 
-}
-
+//calculates the sum of nucleotides
 int dna::sumLength(string input)
 {
-
+    //variables for all the nucleotide counts, total count, error, and line count
     int a_count = 0;
     int c_count = 0;
     int t_count = 0;
@@ -45,13 +37,19 @@ int dna::sumLength(string input)
 
     string strand;
     ifstream infile;
+    //enables reading file
     infile.open(input);
+    //while loop that checks each line of file
     while(infile.peek() != EOF)
     {
+        //increments strandCount
         strandCount++;
         getline(infile, strand);
+
+        //forloop that counts the number of nucleoties
         for(int i = 0; i < strand.size(); ++i)
         {
+            //changes character to lower case
             char charr = tolower(strand[i]);
             if(charr == 'a'){
                 a_count++;
@@ -69,13 +67,16 @@ int dna::sumLength(string input)
                 ++error;
         }
     }
+    //returns total count of nucleoties
     return count;
+    //closes file
     infile.close();
 }
 
+//calculates mean of input file
 double dna::mean(string input)
 {
-
+    //initializes variables for nucleotide counts, errors, and strandCounts
     int a_count = 0;
     int c_count = 0;
     int t_count = 0;
@@ -86,13 +87,19 @@ double dna::mean(string input)
 
     string strand;
     ifstream infile;
+    //opens input file
     infile.open(input);
+    //loops through each line of the file
     while(infile.peek() != EOF)
     {
+        //increments strandcount
         strandCount++;
+        //gets strand from the the file line
         getline(infile, strand);
+        //iterates through and counts total nucleotides
         for(int i = 0; i < strand.size(); ++i)
         {
+            //changes character to lowercase
             char charr = tolower(strand[i]);
             if(charr == 'a'){
                 a_count++;
@@ -110,18 +117,16 @@ double dna::mean(string input)
                 ++error;
         }
     }
+    //calculates mean by dividing total count by amount of lines
     return count/strandCount;
+    //closes file
     infile.close();
 }
 
-
-
-
-
-
-
-string dna::variance(string input)
+//calculates variance for data set
+double dna::variance(string input)
 {
+    //initializes variables
     double count = 0.0;
     int error = 0;
     double strandCount = 0.0;
@@ -132,12 +137,16 @@ string dna::variance(string input)
 
 
     string strand;
+    //reads files
     ifstream infile;
     infile.open(input);
+    //iterates through file line by line
     while(infile.peek() != EOF)
     {
         strandCount++;
+        //gets line from file
         getline(infile, strand);
+        //iterates through strand
         for(int i = 0; i < strand.size(); ++i)
         {
             char charr = tolower(strand[i]);
@@ -152,24 +161,89 @@ string dna::variance(string input)
             else
                 count++;
         }
+
+        //calculates variance
         var = count - av;
         s_var = pow(var,2);
         var_total = var_total + s_var;
+
+        //sets variance variables back to 0
         count = 0;
         var = 0;
         s_var = 0;
     }
+
+    //more variance calculations
     double new_var = var_total / (strandCount-1);
-    double square_var_total = pow(var_total,2);
-    double stdv = square_var_total / (strandCount - 1);
-    string s_new_var = to_string(new_var);
-    string s_stdv = to_string(stdv);
-    return (s_new_var + " and Standard Deviation is: " + s_stdv);
+
+    //returns stdv and variance
+    return new_var;
     infile.close();
 }
 
+
+//calculates stdv for data set
+double dna::stdv(string input)
+{
+    //initializes variables
+    double count = 0.0;
+    int error = 0;
+    double strandCount = 0.0;
+    double av = mean(input);
+    double var_total = 0.0;
+    double var = 0.0;
+    double s_var = 0.0;
+
+
+    string strand;
+    //reads files
+    ifstream infile;
+    infile.open(input);
+    //iterates through file line by line
+    while(infile.peek() != EOF)
+    {
+        strandCount++;
+        //gets line from file
+        getline(infile, strand);
+        //iterates through strand
+        for(int i = 0; i < strand.size(); ++i)
+        {
+            char charr = tolower(strand[i]);
+            if(charr == 'a'){
+                count++;}
+            else if (charr =='c'){
+                count++;}
+            else if (charr =='t'){
+                count++;}
+            else if (charr =='g'){
+                count++;}
+            else
+                count++;
+        }
+
+        //calculates variance
+        var = count - av;
+        s_var = pow(var,2);
+        var_total = var_total + s_var;
+
+        //sets variance variables back to 0
+        count = 0;
+        var = 0;
+        s_var = 0;
+    }
+    double square_var_total = pow(var_total,2);
+    double stdvation = square_var_total / (strandCount - 1);
+
+    return stdvation;
+    infile.close();
+
+}
+
+
+//calculates probability for nucleotides
 string dna::probability(string input)
 {
+    //initializes count variables for each combinations
     double a_count = 0;
     double aa_count = 0;
     double ac_count = 0;
@@ -200,92 +274,144 @@ string dna::probability(string input)
 
     string strand;
     ifstream infile;
+    //reads file
     infile.open(input);
+    //iterates through each line
     while(infile.peek() != EOF)
     {
         strandCount++;
         getline(infile, strand);
-        for(int i = 0; i < strand.size(); ++i)
+        //iterate through each strand
+        for(int i = 0; i < strand.size(); i+=2)
         {
             char charr = tolower(strand[i]);
             char nextcharr = tolower(strand[i+1]);
-
+            //counts each nucleotide combination
             if(charr == 'a'){
                 a_count++;
                 count++;
-                if (nextcharr == 'a')
+                if (nextcharr == 'a'){
                     aa_count++;
-                else if (nextcharr == 'c')
+                    a_count++;
+                    count++;
+                }
+                else if (nextcharr == 'c'){
                     ac_count++;
-                else if (nextcharr == 't')
+                    c_count++;
+                    count++;
+                }
+                else if (nextcharr == 't'){
                     at_count++;
-                else if (nextcharr == 'g')
+                    t_count++;
+                    count++;
+                }
+                else if (nextcharr == 'g'){
                     ag_count++;
+                    g_count++;
+                    count++;
+                }
             }
             else if (charr =='c'){
                 c_count++;
                 count++;
-                if (nextcharr == 'a')
+                if (nextcharr == 'a'){
                     ca_count++;
-                else if (nextcharr == 'c')
+                    a_count++;
+                    count++;
+                }
+                else if (nextcharr == 'c'){
                     cc_count++;
-                else if (nextcharr == 't')
+                    c_count++;
+                    count++;
+                }
+                else if (nextcharr == 't'){
                     ct_count++;
-                else if (nextcharr == 'g')
+                    t_count++;
+                    count++;
+                }
+                else if (nextcharr == 'g'){
                     cg_count++;
+                    g_count++;
+                    count++;
+                }
             }
             else if (charr =='t'){
                 t_count++;
                 count++;
-                if (nextcharr == 'a')
+                if (nextcharr == 'a'){
                     ta_count++;
-                else if (nextcharr == 'c')
+                    a_count++;
+                    count++;
+                }
+                else if (nextcharr == 'c'){
                     tc_count++;
-                else if (nextcharr == 't')
+                    c_count++;
+                    count++;
+                }
+                else if (nextcharr == 't'){
                     tt_count++;
-                else if (nextcharr == 'g')
+                    t_count++;
+                    count++;
+                }
+                else if (nextcharr == 'g'){
                     tg_count++;
+                    g_count++;
+                    count++;
+                }
             }
             else if (charr =='g'){
                 g_count++;
                 count++;
-                if (nextcharr == 'a')
+                if (nextcharr == 'a'){
                     ga_count++;
-                else if (nextcharr == 'c')
+                    a_count++;
+                    count++;
+                }
+                else if (nextcharr == 'c'){
                     gc_count++;
-                else if (nextcharr == 't')
+                    c_count++;
+                    count++;
+                }
+                else if (nextcharr == 't'){
                     gt_count++;
-                else if (nextcharr == 'g')
+                    t_count++;
+                    count++;
+                }
+                else if (nextcharr == 'g'){
                     gg_count++;
+                    g_count++;
+                    count++;
+                }
             }
             else
                 ++error;
         }
     }
 
+    //calculates each percentage for each probability
     double a_prob = (a_count/count)*100;
-    double aa_prob = (aa_count/count)*50;
-    double ac_prob = (ac_count/count)*50;
-    double at_prob = (at_count/count)*50;
-    double ag_prob = (ag_count/count)*50;
+    double aa_prob = (aa_count/count)*200;
+    double ac_prob = (ac_count/count)*200;
+    double at_prob = (at_count/count)*200;
+    double ag_prob = (ag_count/count)*200;
 
     double c_prob = (c_count/count)*100;
-    double ca_prob = (ca_count/count)*50;
-    double cc_prob = (cc_count/count)*50;
-    double ct_prob = (ct_count/count)*50;
-    double cg_prob = (cg_count/count)*50;
+    double ca_prob = (ca_count/count)*200;
+    double cc_prob = (cc_count/count)*200;
+    double ct_prob = (ct_count/count)*200;
+    double cg_prob = (cg_count/count)*200;
 
     double t_prob = (t_count/count)*100;
-    double ta_prob = (ta_count/count)*50;
-    double tc_prob = (tc_count/count)*50;
-    double tt_prob = (tt_count/count)*50;
-    double tg_prob = (tg_count/count)*50;
+    double ta_prob = (ta_count/count)*200;
+    double tc_prob = (tc_count/count)*200;
+    double tt_prob = (tt_count/count)*200;
+    double tg_prob = (tg_count/count)*200;
 
     double g_prob = (g_count/count)*100;
-    double ga_prob = (ga_count/count)*50;
-    double gc_prob = (gc_count/count)*50;
-    double gt_prob = (gt_count/count)*50;
-    double gg_prob = (gg_count/count)*50;
+    double ga_prob = (ga_count/count)*200;
+    double gc_prob = (gc_count/count)*200;
+    double gt_prob = (gt_count/count)*200;
+    double gg_prob = (gg_count/count)*200;
 
     string sa_prob = to_string(a_prob);
     string saa_prob = to_string(aa_prob);
@@ -311,7 +437,7 @@ string dna::probability(string input)
     string sgt_prob = to_string(gt_prob);
     string sgg_prob = to_string(gg_prob);
 
-
+    //returns print statements with each calculated probability
     return ("Probability of 'a': " + sa_prob +  "%\n" + "Probability of 'aa': " + saa_prob +  "%\n" + "Probability of 'ac': " + sac_prob +  "%\n" + "Probability of 'at': " + sat_prob +  "%\n" + "Probability of 'ag': " + sag_prob +  "%\n" +   "Probability of 'c': " + sc_prob + "%\n" +  "Probability of 'ca': " + sca_prob +  "%\n" + "Probability of 'cc': " + scc_prob +  "%\n" + "Probability of 'ct': " + sct_prob +  "%\n" + "Probability of 'cg': " + scg_prob +  "%\n"  "Probability of 't': " + st_prob + "%\n" +  "Probability of 'ta': " + sta_prob +  "%\n" + "Probability of 'tc': " + stc_prob +  "%\n" + "Probability of 'tt': " + stt_prob +  "%\n" + "Probability of 'tg': " + stg_prob +  "%\n"  "Probability of 'g': " + sg_prob + "%\n" +  "Probability of 'ga': " + sga_prob +  "%\n" + "Probability of 'gc': " + sgc_prob +  "%\n" + "Probability of 'gt': " + sgt_prob +  "%\n" + "Probability of 'gg': " + sgg_prob +  "%\n" );
     infile.close();
 }
--- dna_main.cpp
+++ dna_main.cpp
@@ -1,28 +1,43 @@
+//header file that declares all the files
 #include "dna.h"
+//allows for reading and writing files
 #include <iostream>
 #include <fstream>
+#include <random>
+#include <iomanip>
+#include <string>
+#include <map>
+#include <cmath>
 
+
+//allows you to print
 using namespace std;
 
 int main(int argc, char** argv) //argc stands for command line arguments
 //argv number arguments in command line
 {
+    //creates file to write out to
     ofstream outputFile;
     outputFile.open("davidaarhus.txt");
 
-    outputFile << argv[1];
+    //stores command line argument; acts as scanner class from java
     string file_name = argv[1];
 
-    dna d; //stored in the stack
+    //stored in the stack
+    dna d;
 
-    outputFile << "Strings in file: " << "\n---------------" << d.countLength(file_name) << endl;
+    //writes out all the calculations to a new file
+    outputFile << "David Aarhus\n2291228\nCPSC 350-01\n\n";
     outputFile << "Sum of DNA nucleotides: " << d.sumLength(file_name) << endl;
     outputFile << "Average amount of nucleotides in each DNA strand: " << d.mean(file_name) << endl;
     outputFile << "Variance: " << d.variance(file_name) << endl;
+    outputFile << "Standard Deviation: " << d.stdv(file_name) << endl;
     outputFile << d.probability(file_name) << endl;
 
-
+    // tells user that program was completed
     cout << "progam exited successfully" << endl;
+
+    //closes file
     outputFile.close();
 
     return 0;
